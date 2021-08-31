55745e89203ffc3cea7d870c5272e55f1011f542
maxgm <maxgarro22@estudiantec.cr>
2021-08-30 19:08:19 -0600

    [Update] Inicio

---
+++ cmake-build-debug/CMakeFiles/Proyecto1_D2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto1_D2.dir/main.cpp.o"
+  "Proyecto1_D2"
+  "Proyecto1_D2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto1_D2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
